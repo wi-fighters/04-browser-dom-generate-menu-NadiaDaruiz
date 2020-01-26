2d66807f6094931bde72b3aa17703c96cf10d9bb
Eva Larumbe <evalarumbe@gmail.com>
2020-01-26 22:56:54 +0100

    Get hrefs and link text programmatically

--- script.js
+++ script.js
@@ -25,30 +25,28 @@
     const ul = document.createElement('ul');
     
     // Which tool do we use to iterate over these steps?
-    // We don't need a new array, so forEach works for us
+    // Maybe map? Well, we don't need a new array, so forEach works for us this time
     
     ids.forEach(id => {
         const li = document.createElement('li');
     
         const link = document.createElement('a');
-        link.setAttribute('href', '#home'); // TODO: get this programmatically
-        // Maybe use something like...
-        // const hrefs = ids.map(id => '#' + id);
+        link.setAttribute('href', `#${id}`);
     
-        const linkText = document.createTextNode('Home'); // TODO: get this programmatically
+        const linkText = document.createTextNode(id[0].toUpperCase() + id.substring(1).toLowerCase());
     
         link.prepend(linkText);
     
         li.prepend(link);
     
-        ul.prepend(li);
+        ul.appendChild(li);
     });
         
     header.prepend(ul);
     
     // SEE THE RESULT
     
-    // Once you're built the whole subtree in memory,
-    // Add it to the DOM (define the relationship between the two things)
+    // Once you've built the whole subtree in memory,
+    // Add it to the DOM
     body.prepend(header);    
 })();
